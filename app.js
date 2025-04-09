5ffadc4736130471f2434bdb40ad41b52f772a8a
milanbhatiya59 <milanbhatiya59@gmail.com>
2025-04-09 20:49:13 +0530

    remove cors credentials

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 const CORS_ORIGIN = 'https://agrisolution.vercel.app';
 
-app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
